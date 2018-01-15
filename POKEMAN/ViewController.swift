c14ae04ec6f11cc33d5f10079dcc463f6605b6cd
Bharat Khatke <Bharat@innovages-imac.local>
2018-01-15 18:33:49 +0530

    Initial Commit

---
+++ POKEMAN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POKEMAN
+//
+//  Created by Bharat Khatke on 15/01/18.
+//  Copyright © 2018 Bharat Khatke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
